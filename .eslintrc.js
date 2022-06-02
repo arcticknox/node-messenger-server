631f7dc98901b0f9a3707a7c08277862b41ff291
Kaushik Gnanasekar <kaushikgnanasekar@gmail.com>
2022-06-02 13:31:08 +0530

    added eslintrc, gitignore

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  'env': {
+    'node': true,
+    'browser': true,
+    'commonjs': true,
+    'es2021': true
+  },
+  'extends': 'eslint:recommended',
+  'parserOptions': {
+    'ecmaVersion': 'latest'
+  },
+  'rules': {
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'always'],
+    'indent': ['error', 2],
+    'no-multi-spaces': ['error']
+  }
+};
