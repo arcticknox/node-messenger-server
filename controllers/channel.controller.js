2d18341fc662d5852f20e12c5a5b744735668895
Kaushik Gnanasekar <kaushikgnanasekar@gmail.com>
2022-06-04 18:18:43 +0530

    changed error message

--- controllers/channel.controller.js
+++ controllers/channel.controller.js
@@ -6,7 +6,7 @@ const ApiError = require('../utils/ApiError');
 
 const create = catchAsync(async (req, res) => {
   const { name, members } = req.body;
-  if (name === 'Lobby') throw new ApiError(httpStatus.FORBIDDEN, 'Channel name now allowed');
+  if (name === 'Lobby') throw new ApiError(httpStatus.FORBIDDEN, 'Forbidden channel name');
   const ownerId = _.get(req, 'user.id');
   const channel = await ChannelService.createChannel(ownerId, name, members);
   res.status(httpStatus.CREATED).send(channel);
