398193868179f3bb679081521cb9df9ced299ea2
Kaushik Gnanasekar <kaushikgnanasekar@gmail.com>
2022-06-15 18:59:42 +0530

    added namespace for messages

--- middlewares/socket.js
+++ middlewares/socket.js
@@ -2,12 +2,13 @@ const { sendMessage } = require('../services/socket.service');
 const logger = require('../config/logger');
 
 module.exports.socketIO = async (io) => {
+  const messenger = io.of('/messenger');
   let connections = 0;
-  io.on('connection', async (socket) => {
+  messenger.on('connection', async (socket) => {
     connections +=1;
     logger.info(`${socket.id} has connected! Connections: ${connections}`);
     // sendMessage event
-    socket.on('sendMessage', payload => sendMessage(payload, io));
+    socket.on('sendMessage', payload => sendMessage(payload, messenger));
 
   });
   
