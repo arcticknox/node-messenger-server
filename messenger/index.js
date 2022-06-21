7097c45dd1b5220dbd8743fe0e9e627fe09ec9ed
Kaushik Gnanasekar <kaushikgnanasekar@gmail.com>
2022-06-21 12:46:43 +0530

    socket.io rooms support for messenger (#17)
    * working socket rooms integration for messenger
    
    * some cleanup

--- messenger/index.js
+++ messenger/index.js
@@ -1,4 +1,4 @@
-const { sendMessage } = require('./messenger');
+const { sendMessage, initRooms } = require('./messenger');
 const logger = require('../config/logger');
 const { messengerEvents } = require('../config/events');
 
@@ -7,5 +7,7 @@ module.exports.connectMessenger = async (io) => {
     logger.info(`${socket.id} has connected to messenger socket!`);
     // sendMessage event
     socket.on(messengerEvents.sendMessage, payload => sendMessage(payload, io));
+    // Init rooms
+    socket.on('common:init-rooms', payload => initRooms(payload, socket));
   });
 };
