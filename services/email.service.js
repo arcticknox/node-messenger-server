f1cacfbaafa5e22755c95d60453430a4bda9d4bc
kuckchuck96 <50999920+kuckchuck96@users.noreply.github.com>
2022-08-13 13:24:51 +0530

    Changed resetPassword URL

--- services/email.service.js
+++ services/email.service.js
@@ -31,8 +31,7 @@ const sendEmail = async (to, subject, text) => {
  */
 const sendResetPasswordEmail = async (to, token) => {
   const subject = `${config.appName}${config.env === 'dev' ? '-' + config.env : ''}: Reset password`;
-  // replace this url with the link to the reset password page of your front-end app
-  const resetPasswordUrl = `${config.email.verifyDomain}/v1/auth/reset-password?token=${token}`;
+  const resetPasswordUrl = `${config.env === 'dev' ? config.email.verifyDomain.replace('3000', '4200') : config.email.verifyDomain}/login/reset-password/${token}`;
   const text = `Dear user,\n
 To reset your password, click on this link: ${resetPasswordUrl}
 If you did not request any password resets, then ignore this email.`;
