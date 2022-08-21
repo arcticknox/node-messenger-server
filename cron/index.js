6e280343a7e063d6bee7d2bdefbe247ad43f35e1
kuckchuck96 <50999920+kuckchuck96@users.noreply.github.com>
2022-08-21 21:56:44 +0530

    Fixed cron issue

--- cron/index.js
+++ cron/index.js
@@ -1,44 +1,16 @@
 const cron = require('node-cron');
-const config = require('../config/config');
 const logger = require('../config/logger');
-const { deleteMessages } = require('../services/message.service');
+const { deleteOldMessages } = require('./message.cron');
 
-let tasks = [];
+const crons = [
+  deleteOldMessages
+];
 
-/**
- * Deletes specified days old messages
- * based on MESSAGE_RETENTION env variable
- */
-tasks.push(cron.schedule('* * 20 * * *', async () => {
-    logger.info('***** Started: Delete Old Messages *****')
-
-    let date = new Date();
-    date.setDate(date.getDate() - config.messageRetention);
-
-    const {acknowledged, deletedCount} = await deleteMessages({
-        createdAt: {
-            $lt: new Date(date.getFullYear(), date.getMonth(), date.getDay())
-        }
-    });
-
-    if (acknowledged) {
-        logger.info(`Total ${deletedCount} messages deleted.`)
-    }
-    logger.info('***** Ended: Delete Old Messages *****')
-}, {
-    scheduled: false,
-    timezone: 'Asia/Kolkata'
-}));
-
-const startCronTasks = () => {
-    tasks.forEach(t => t.start());
+const scheduleCrons = () => {
+  logger.info(`Started scheduling ${crons.length} crons.`);
+  crons.forEach(c => cron.schedule(c.schedule, c.function));
 };
 
-const stopCronTasks = () => {
-    tasks.forEach(t => t.stop());
-}
-
 module.exports = {
-    startCronTasks,
-    stopCronTasks
+  scheduleCrons,
 };
