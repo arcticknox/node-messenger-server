0a7209b2ecaf5bba626649c96cbe2dc279a50f11
Kaushik Gnanasekar <kaushikgnanasekar@gmail.com>
2022-08-22 13:39:55 +0530

    mediasoup fixed build issues

--- buildAndPush.sh
+++ buildAndPush.sh
@@ -4,11 +4,12 @@ branch=$(git symbolic-ref -q HEAD)
 branch=${branch##refs/heads/}
 branch=${branch:-HEAD}
 version=$1
+image=838494801100.dkr.ecr.ap-south-1.amazonaws.com/messenger-server
 
 echo "Using branch ${branch}"
 echo "Building image..."
 docker build -t messenger-server:${version:-$branch} .
 echo "Logging into ECR..."
 aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 838494801100.dkr.ecr.ap-south-1.amazonaws.com
-docker tag messenger-server:${version:-$branch} 838494801100.dkr.ecr.ap-south-1.amazonaws.com/messenger-server:${version:-$branch}
-docker push 838494801100.dkr.ecr.ap-south-1.amazonaws.com/messenger-server:${version:-$branch}
+docker tag messenger-server:${version:-$branch} ${image}:${version:-$branch}
+docker push ${image}:${version:-$branch}
